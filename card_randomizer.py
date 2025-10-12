028da980ac51e772432ac77609d910ede28c697e
Maiss <matiss.sterns@gmail.com>
2025-10-12 17:38:58 +0300

    Add card deck management and scoring to Blackjack
    Added new images for background and board. Enhanced card_randomizer.py with functions to add and remove cards from the deck, ensuring unique cards. Updated test.py to support hit/stand actions, score calculation, and improved UI elements for a more interactive Blackjack game.

--- card_randomizer.py
+++ card_randomizer.py
@@ -1,9 +1,43 @@
 import random
-numbers=["2","3","4","5","6","7","8","9","10","ace","jack","king","queen"]
-signs=["c","d","h","s"]
+
+numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "ace", "jack", "king", "queen"]
+signs = ["c", "d", "h", "s"]
+
+max_cards=len(numbers)*len(signs)
+
+
 def shuffle(Card_count):
     cards = []
-    for i in range(0,Card_count):
-        card=random.choice(numbers) +"_of_" + random.choice(signs)
-        cards.append(card)
-    return(cards)
+    done = 0
+    for i in range(0, Card_count):
+        while done < Card_count:
+            card = random.choice(numbers) + "_of_" + random.choice(signs)
+            if card in cards:
+                pass
+            else:
+                cards.append(card)
+                done = done + 1
+    return cards
+
+
+def shuffle_add(prev_deck):
+    cards = prev_deck
+    if len(cards)<max_cards:
+        done = 0
+        while done ==0:
+            card = random.choice(numbers) + "_of_" + random.choice(signs)
+            if card in cards:
+                pass
+            else:
+                cards.append(card)
+                done=1
+    else:
+        print("All cards used")
+    return cards
+
+
+def shuffle_remove(prev_deck):
+    cards = prev_deck
+    if len(cards)>=1:
+        cards.pop()
+    return cards
--- test.py
+++ test.py
@@ -1,22 +1,27 @@
 import pygame
 import sys
-from card_randomizer import shuffle
+from card_randomizer import shuffle, shuffle_add, shuffle_remove
 
 pygame.init()
 running = True
 
 # window settings
-screen_width = 900
+screen_width = 1000
 screen_hight = 600
 screen = pygame.display.set_mode((screen_width, screen_hight))
 clock = pygame.time.Clock()
 pygame.display.set_caption("BlackJack")
 
 
+specialcardvalues = {"a": 11, "j": 10, "k": 10, "q": 10}
+
 image_card = pygame.image.load("Cards/Empty.png")
 deck = []
 shuffle_deck = True
 card_scale = 0.25
+
+score = 0
+
 
 # text font
 font = pygame.font.Font("Images/JMH Typewriter-Black.otf", 30)
@@ -57,11 +62,19 @@ def draw_image(file, x, y, scale):
 
 # Game looop
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((60, 150, 60))
+    screen.fill((37, 105, 21))
+    # draw_image("Images/background.jpg", 0, 0,0.37)
     mouse_pos = pygame.mouse.get_pos()
+    mouse_pressed = pygame.mouse.get_pressed()[0]
+
+    # reset pressed buttons
+    if not mouse_pressed:
+        hit = False
+        stand = False
     # Loop code
 
     # Shuffle cards (once)
@@ -86,6 +99,11 @@ while running:
     if hitb_rect.collidepoint(mouse_pos):
         draw_image("Images/wood_button_dark.png", hitb_x, hitb_y, hitb_size)
         draw_text("Hit", hitb_x, hitb_y, hitb_w, hitb_h, 0, 0, 0)
+        if mouse_pressed and not hit:
+            print("Hit")
+            hit = True
+            deck = shuffle_add(deck)
+
     else:
         draw_image("Images/wood_button.png", hitb_x, hitb_y, hitb_size)
         draw_text("Hit", hitb_x, hitb_y, hitb_w, hitb_h, 50, 20, 20)
@@ -93,9 +111,24 @@ while running:
     if standb_rect.collidepoint(mouse_pos):
         draw_image("Images/wood_button_dark.png", standb_x, standb_y, standb_size)
         draw_text("Stand", standb_x, standb_y, standb_w, standb_h, 0, 0, 0)
+        if mouse_pressed and not stand:
+            print("Stand")
+            deck = shuffle_remove(deck)
+            stand = True
     else:
         draw_image("Images/wood_button.png", standb_x, standb_y, standb_size)
         draw_text("Stand", standb_x, standb_y, standb_w, standb_h, 50, 20, 20)
+
+    draw_image("Images/wood_board.png", 830, 360, 0.16)
+    draw_text(str(round(score)), 830, 360, 169, 169, 51, 20, 9)
+
+    # score counter
+    score = 0
+    for i in range(len(deck)):
+        if deck[i][0] in ["a", "j", "k", "q"]:
+            score = score + specialcardvalues.get(deck[i][0])
+        else:
+            score = score + float(deck[i][0])
 
     # Loop end
     dt = clock.tick(60) / 1000
